ec5054326238f57e27cc66be6dae421c150b57d4
Wanghaizhou <geekberry@qq.com>
2020-04-07 14:08:57 +0800

    UPDATE: error to LogicError

---
+++ packages/koaflow/lib/util/LogicError.js
@@ -0,0 +1,31 @@
+class LogicError extends Error {
+  static extend({ name, ...options } = {}) {
+    class ExtendError extends this {
+      static get name() {
+        return name || super.name;
+      }
+
+      constructor(arg) {
+        super(arg);
+        Object.assign(this, options);
+      }
+    }
+
+    Object.assign(ExtendError, options);
+    return ExtendError;
+  }
+
+  constructor(arg) {
+    super();
+    this.name = this.constructor.name;
+
+    if (arg instanceof Error) {
+      this.message = arg.message;
+      this.stack = arg.stack; // 复制调用栈, (stack 不会被设为 Object.keys)
+    } else if (typeof arg === 'string') {
+      this.message = arg;
+    }
+  }
+}
+
+module.exports = LogicError;
--- packages/koaflow/test/LogicError.test.js
+++ packages/koaflow/test/LogicError.test.js
@@ -1,23 +1,29 @@
-const LogicError = require('../src/error');
+const LogicErrorTest = require('../lib/util/LogicError');
 
 test('extend LogicError', () => {
-  const XError = LogicError.extend({ code: 1001, stack: 'stack' });
+  const XError = LogicErrorTest.extend({ code: 1001, stack: 'stack' });
+  expect(XError.name).toEqual('LogicError');
+  expect(XError.code).toEqual(1001);
+  console.log(XError.stack);
 
   const e = new XError('x error');
-
   expect(e.name).toEqual('LogicError');
   expect(e.code).toEqual(1001);
   expect(e.message).toEqual('x error');
+  console.log(e.stack);
 
   expect({ ...e }).toEqual({ name: 'LogicError', code: 1001, message: 'x error' });
 });
 
 test('copy constructor', () => {
-  const TestError = LogicError.extend({ name: 'TestError', code: 9999 });
+  const TestError = LogicErrorTest.extend({ name: 'TestError', code: 9999 });
+  expect(TestError.name).toEqual('TestError');
+  expect(TestError.code).toEqual(9999);
 
-  const e = new Error('normal error');
-  const eTest = new TestError(e);
-
-  expect(e.message).toEqual(eTest.message);
-  expect(e.stack).toEqual(eTest.stack);
+  const error = new Error('normal error');
+  const e = new TestError(error);
+  expect(e.name).toEqual('TestError');
+  expect(e.code).toEqual(9999);
+  expect(e.message).toEqual(error.message);
+  expect(e.stack).toEqual(error.stack);
 });
